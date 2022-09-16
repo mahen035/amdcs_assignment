78fd6290c6270a191e78b6e8696aa1dbd92651d7
amdjfsim01 <amdjfsim01@AmdJava01.iiht.tech>
2022-09-16 11:43:00 +0530

    initial commit

---
+++ src/com/training/amdcs/assignment1/MyClass.java
@@ -0,0 +1,10 @@
+package com.training.amdcs.assignment1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world! this is my first assignment");
+
+	}
+
+}
